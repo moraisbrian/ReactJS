85dcb18984ef8265f28dba6a8c91dbf62ebd62f4
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-01-03 18:42:59 -0300

    Aula 21

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -4,22 +4,26 @@ import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
+import './App.css';
 
 const app = () =>
-    <div>
+    <div className="App">
         <h1>Fundamentos React</h1>
-        <Card titulo="Desafio Aleatório">
-            <Aleatorio min={1} max={10} />
-        </Card>
-        <Card titulo="Fragmento">
-            <Fragmento />
-        </Card>
-        <Card titulo="Com Parâmetro">
-            <ComParametro titulo="Situação do Aluno" aluno="Marcos" nota={8.6} />
-        </Card>
-        <Card titulo="Primeiro Componente">
-            <Primeiro />
-        </Card>
+
+        <div className="Cards">
+            <Card titulo="Desafio Aleatório" color="#080">
+                <Aleatorio min={1} max={10} />
+            </Card>
+            <Card titulo="Fragmento" color="#00008B">
+                <Fragmento />
+            </Card>
+            <Card titulo="Com Parâmetro" color="#20B2AA">
+                <ComParametro titulo="Situação do Aluno" aluno="Marcos" nota={8.6} />
+            </Card>
+            <Card titulo="Primeiro Componente" color="#F0E68C">
+                <Primeiro />
+            </Card>
+        </div>
     </div>
 
 export default app;

471bb849e33c383d9df0c17b8beda63e5f6fc92d
Brian Leonardo de Morais <morais.brian360@gmail.com>
2021-01-12 09:12:31 -0300

    Aula 41

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+export default class Calculator extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Calculator</h1>
+                <div className="calculator">
+                
+                </div>
+            </div>
+            
+        );
+    }
+}

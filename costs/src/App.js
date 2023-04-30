085baa4b79732c2c97047ff9d76a165e18d2f0e0
Brian Leonardo de Morais <morais.brian360@gmail.com>
2023-04-30 08:50:37 -0300

    Adicionado projeto Costs

---
+++ costs/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Projects from './components/pages/Projects';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/newproject' element={<NewProject />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ costs/src/components/layout/Footer.jsx
@@ -0,0 +1,25 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <ul className="social_list">
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+            </ul>
+            <p className="copy_right">
+                <span>Costs</span> &copy; 2023
+            </p>
+        </footer>
+    );
+}
+
+export default Footer
